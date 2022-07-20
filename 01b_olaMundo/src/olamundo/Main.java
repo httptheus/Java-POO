b8be72de48bb90e62dddb6cfe75e2b0abf59edb8
httptheus <httptheus404@gmail.com>
2022-07-20 18:01:19 -0300

    Commit 20/07/2022

--- 01b_olaMundo/src/olamundo/Main.java
+++ 01b_olaMundo/src/olamundo/Main.java
@@ -3,7 +3,7 @@ package olamundo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá, mundo!");
+		System.out.println("Olá, mundo!"); //Escrever 'syso' + ctrl+espaço exibe o comando de escrever no console.
 	}
 
 }
