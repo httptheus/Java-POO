7ecb9e9149ac13cee6e6f532d9af75fb8e1d47ca
httptheus <httptheus404@gmail.com>
2022-07-20 18:03:25 -0300

    Commit 20/07/2022

--- 01b_olaMundo/src/olamundo/Main.java
+++ 01b_olaMundo/src/olamundo/Main.java
@@ -1,6 +1,6 @@
 package olamundo;
 
-public class Main {
+public class Main { //Main é classe onde será executada todas as classes e suas instâncias, interfaces e etc. Todo projeto deverá ter um arquivo .java principal.
 
 	public static void main(String[] args) {
 		System.out.println("Olá, mundo!"); //Escrever 'syso' + ctrl+espaço exibe o comando de escrever no console.
